8ef4a89ea95e35df80e503b4872e75dabd88e40c
Rupam <hrupam55@gmail.com>
2020-08-06 20:25:48 +0530

    Removed animate.css and added media query

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 $(document).ready(() => {
-    $(".burger").addClass("animate__animated animate__rubberBand");
-    $(".content h4").addClass("animate__animated animate__heartBeat");
-    $(".content p").addClass("animate__animated animate__wobble");
     $(".burger").click(() => {
-        if($(".burger").children("i").hasClass("fa-bars")){
+        if ($(".burger").children("i").hasClass("fa-bars")) {
             $(".burger").children("i").removeClass("fa-bars");
             $(".burger").children("i").addClass("fa-times");
         }
-        else{
+        else {
             $(".burger").children("i").removeClass("fa-times");
             $(".burger").children("i").addClass("fa-bars");
         }
