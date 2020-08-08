50a2067e1cab07d2ef78ce31d0f861ae8df34e55
Rupam <hrupam55@gmail.com>
2020-08-09 02:53:59 +0530

    Formatted Documents

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 $(document).ready(() => {
-    $(".burger").click(() => {
-        if ($(".burger").children("i").hasClass("fa-bars")) {
-            $(".burger").children("i").removeClass("fa-bars");
-            $(".burger").children("i").addClass("fa-times");
-        }
-        else {
-            $(".burger").children("i").removeClass("fa-times");
-            $(".burger").children("i").addClass("fa-bars");
-        }
-        $(".nav").toggleClass("nav-active");
-        $(".content").toggleClass("content-collapse");
-    })
-})
+  $(".burger").click(() => {
+    if ($(".burger").children("i").hasClass("fa-bars")) {
+      $(".burger").children("i").removeClass("fa-bars");
+      $(".burger").children("i").addClass("fa-times");
+    } else {
+      $(".burger").children("i").removeClass("fa-times");
+      $(".burger").children("i").addClass("fa-bars");
+    }
+    $(".nav").toggleClass("nav-active");
+    $(".content").toggleClass("content-collapse");
+  });
+});
